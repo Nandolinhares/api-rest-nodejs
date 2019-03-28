72f4e15a415f06adb9aadc5750fad38390845aa2
Nando Linhares <fernandolinhares1920@globo.com>
2019-03-28 11:58:32 -0300

    Created Model Product

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const mongoose = require('mongoose');
 const app = express();
 
 //Iniciando DB
+mongoose.connect('mongodb://localhost:1111/api-rest', { useNewUrlParser: true } );
 
+app.get('/', (req, res) => {
+    res.send("Hello World");
+});
 
 app.listen(3001);
