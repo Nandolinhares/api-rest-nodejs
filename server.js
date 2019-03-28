367432b5bd7483a6482b9d096c976694afab8f22
Nando Linhares <fernandolinhares1920@globo.com>
2019-03-28 19:52:48 -0300

    cors added

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
+const cors = require('cors');
 
 //Iniciando o app
 const app = express();
 
 app.use(express.json()); //Permite que eu envie dados a minha apps usando JSON
+app.use(cors());
 
 //Iniciando DB
 mongoose.connect('mongodb://localhost:1111/api-rest', { useNewUrlParser: true } );
@@ -14,8 +16,5 @@ requireDir('./src/models');
 
 app.use('/api', require("./src/routes"));
 
-app.get('/', (req, res) => {
-   res.send("Hello World");
-});
 
 app.listen(3001);
