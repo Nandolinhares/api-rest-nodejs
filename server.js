a2f644055a985e2728ac569023b075d5578fdb84
Nando Linhares <fernandolinhares1920@globo.com>
2019-03-28 18:38:35 -0300

    Created first controller, routes and require in the server

--- server.js
+++ server.js
@@ -5,21 +5,17 @@ const requireDir = require('require-dir');
 //Iniciando o app
 const app = express();
 
+app.use(express.json()); //Permite que eu envie dados a minha apps usando JSON
+
 //Iniciando DB
 mongoose.connect('mongodb://localhost:1111/api-rest', { useNewUrlParser: true } );
 
 requireDir('./src/models');
 
-const Product = mongoose.model('product');
+app.use('/', require("./src/routes"));
 
 app.get('/', (req, res) => {
-    Product.create({
-        title: 'React Native',
-        description: 'É uma tecnologia para desenvolver apps mobile',
-        url: 'https://github.com/facebook/react-native'
-    });
-
-    return res.send("Hello World");
+   res.send("Hello World");
 });
 
 app.listen(3001);
